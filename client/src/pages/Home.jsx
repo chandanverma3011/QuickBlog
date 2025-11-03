f8a4ac74de32241f4f7283aebb77a9885fb7fa42
chandanverma3011 <chandanverma3011@gmail.com>
2025-11-04 00:49:30 +0530

    UI/UX changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,13 +4,21 @@ import Header from '../components/Header'
 import Bloglist from '../components/Bloglist'
 import Newsletter from '../components/Newsletter'
 import Footer from '../components/Footer'
+import FeaturedStrip from '../components/FeaturedStrip'
+import TrendingSidebar from '../components/TrendingSidebar'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Header/>
-    <Bloglist/>
+    <FeaturedStrip/>
+    <div className='mx-8 sm:mx-16 xl:mx-24 flex gap-8'>
+      <div className='flex-1'>
+        <Bloglist/>
+      </div>
+      <TrendingSidebar/>
+    </div>
     <Newsletter/>
     <Footer/>
 
--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -17,11 +17,11 @@ const layout = () => {
 
   return (
     <>
-    <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
+    <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-border bg-surface'>
         <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={()=> navigate('/')} />
-        <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'>Logout</button>
+        <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer shadow hover:shadow-primary/30 transition'>Logout</button>
     </div>
-    <div className='flex h-[calc(100vh-70px)]'>
+    <div className='flex h-[calc(100vh-70px)] bg-bg text-text'>
         <Sidebar/>
         <Outlet/>
     </div>
